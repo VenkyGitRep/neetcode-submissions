78612535fa3c0064a01be9c3eaadf85380162abd
Venkateshwaran Sundar <106343437+VenkyGitRep@users.noreply.github.com>
2026-06-04 23:42:53 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagramMap = {}
+        for s in strs:
+            key = self.getKey(s)
+            if key in anagramMap:
+                anagramMap[key].append(s)
+            else:
+                anagramMap[key] = [s]
+        return list(anagramMap.values())
+        
+    
+    def getKey(self, s:str)->tuple:
+        key = [0]*26
+        for c in s:
+            key[ord(c)-ord('a')]+=1
+        return tuple(key)
+
+
+
+
+
